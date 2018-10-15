802b046a753f5dea2dadb1313946a81c65220112
Rafelck <rafelinoxz7@gmail.com>
2018-10-16 00:46:14 +0800

    add view

--- application/controllers/Ac.php
+++ application/controllers/Ac.php
@@ -3,12 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Ac extends CI_Controller {
 
-	/*function __construct() {
+	function __construct() {
         parent::__construct();
         $this->load->model('am');
-    }*/
+    }
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('d_lunas');
 	}
 }
---
+++ application/views/Component/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>PRINTER MANAGEMENT</title>
+
+    <!-- Bootstrap core CSS-->
+    <link href="resource/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template-->
+    <link href="resource/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Page level plugin CSS-->
+    <link href="resource/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="resource/css/sb-admin.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
+
+      <a class="navbar-brand mr-1" href="index.html">Printer Management</a>
+
+      <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
+        <i class="fas fa-bars"></i>
+      </button>
+
+
+    </nav>
+
+    <div id="wrapper">
+
+      <!-- Sidebar -->
+      <ul class="sidebar navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Daftar Lunas</span>
+          </a>
+        </li>
+      </ul>
